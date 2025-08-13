d61398b762858fc6e2c4f34c3e598043986622b0
Ram Singh <ramxv.singh@gmail.com>
2025-08-13 13:31:00 -0500

    feat: fix card services content

--- src/components/content/ModalService.tsx
+++ src/components/content/ModalService.tsx
@@ -10,18 +10,26 @@ import {
   DialogTrigger,
 } from "@/components/ui/dialog";
 
-export const ModalService = ({ title, description, content }: { title: string, description: string, content: string }) => {
+export const ModalService = ({ title, description, image, content }: { title: string, description: string, image: string, content: string }) => {
   return (
     <Dialog>
       <DialogTrigger asChild>
-        <h1>{title}</h1>
+        <h1 >{title}</h1>
       </DialogTrigger>
-      <DialogContent className="sm:max-w-[425px]">
+      <DialogContent className="w-full">
         <DialogHeader>
-          <DialogTitle>{title}</DialogTitle>
-          <DialogDescription>{description}</DialogDescription>
+          <DialogTitle className="font-bold uppercase text-2xl">{title}</DialogTitle>
+          <DialogDescription className="font-principal">{description}</DialogDescription>
         </DialogHeader>
-        <p>{content}</p>
+        <div className="relative w-full overflow-hidden rounded-lg
+                  aspect-[4/3] sm:aspect-video md:aspect-[3/2]">
+          <img
+            src={image}
+            alt=""
+            className="absolute inset-0 h-full w-full object-cover object-center transition-all duration-200"
+          />
+        </div>
+        <p className="font-principal prose">{content}</p>
         <DialogFooter>
           <DialogClose asChild>
             <Button>Cerrar</Button>
